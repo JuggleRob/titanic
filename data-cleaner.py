00630f3e2292cd555fd95b85a3f0b5d8fc796c54
Rob <rob@pop-os.localdomain>
2021-12-25 15:32:42 +0100

    Added the features FamilySize and IsAlone

--- data-cleaner.py
+++ data-cleaner.py
@@ -1,29 +1,28 @@
 import pandas as pd 
+from prompt_toolkit import prompt
+
+input = prompt("Enter 1 to clean the training dataset. Enter 2 to clean the test dataset. Choice: ")
+if input == "1":
+    url = "titanic-training-dataset"
+else:
+    url = "titanic-test-dataset"
 
 # Import dataframe
-df = pd.read_csv('titanic-training-dataset.csv')
-
-# Clean Data
-
-# Change Survived datatype from int to bool
-df['Survived'] = df['Survived'].astype('bool')
-
-# Remove two rows with null value at Embarked
+df = pd.read_csv(url + ".csv")
 
 # Create Title from Name column and remove Name column
+# 1 = Miss , 2 = Mrs , 3 = Mr , 4 = Master , 5 = Other
 df['Title'] = df['Name'].str.split(',', expand = True)[1]
 df['Title'] = df['Title'].str.split(' ', expand = True)[1]
-df['Title'] = df['Title'].replace(['Lady.', 'Countess.','Capt.', 'Col.','Dr.', 'Major.', 'Rev.', 'Sir.', 'Don.', 'Jonkheer.'], 'Other.')
-df['Title'] = df['Title'].replace('Mlle.', 'Miss.')
-df['Title'] = df['Title'].replace('Ms.', 'Miss.')
-df['Title'] = df['Title'].replace('Mme.', 'Mrs.')
+df['Title'] = df['Title'].replace(['Mlle.', 'Ms.', 'Miss.'], 1)
+df['Title'] = df['Title'].replace(['Mrs.', 'Mme.'], 2)
+df['Title'] = df['Title'].replace('Mr.', 3)
+df['Title'] = df['Title'].replace('Master.', 4)
+df['Title'] = df['Title'].replace(['Lady.', 'Countess.','Capt.', 'Col.','Dr.', 'Major.', 'Rev.', 'Sir.', 'Don.', 'Jonkheer.', 'Dona.'], 5)
 
-
-# Change Pclass to categories
-df['Pclass'] = df['Pclass'].astype('category')
-df['Embarked'] = df['Embarked'].astype('category')
-df['Sex'] = df['Sex'].astype('category')
-df['Title'] = df['Title'].astype('category')
+# Encode Sex to: female = 0 , male = 1
+df['Sex'] = df['Sex'].replace('female', 0)
+df['Sex'] = df['Sex'].replace('male', 1)
 
 # Impute age based on Pclass
 def impute_age(age_pclass):
@@ -42,39 +41,57 @@ def impute_age(age_pclass):
 # Data analysis
 df['Age'] = df[['Age', 'Pclass']].apply(impute_age, axis=1)
 
-def age_groups(x):
-    if x < 10:
-        return '0-10 years'
-    elif x < 20:
-        return '10-20 years'
-    elif x < 30:
-        return '20-30 years'
-    elif x < 40:
-        return '30-40 years'
-    elif x < 50:
-        return '40-50 years'
-    elif x < 60:
-        return '50-60 years'
+def age_groups(age):
+    if age < 10:
+        return 0 # 0-10 years 
+    elif age < 20:
+        return 1 # 10-20 years
+    elif age < 30:
+        return 2 # 20-30 years 
+    elif age < 40:
+        return 4 # 30-40 years
+    elif age < 50:
+        return 5 # 40-50 years
+    elif age < 60:
+        return 6 # 50-60 years
     else:
-        return '>60 years'
+        return 7 # >60 years
 
-def fare_groups(x):
-    if x < 10:
-        return '0-10 pounds'
-    elif x < 20:
-        return '10-20 pounds'
-    elif x < 30:
-        return '20-30 pounds'
-    elif x < 40:
-        return '30-40 pounds'
-    elif x < 50:
-        return '40-50 pounds'
-    elif x < 100:
-        return '50-100 pounds'
+def fare_groups(fare):
+    if fare < 10:
+        return 1 # 0-10 pounds
+    elif fare < 20:
+        return 2 # 10-20 pounds
+    elif fare < 30:
+        return 3 # 20-30 pounds
+    elif fare < 40:
+        return 3 # 30-40 pounds
+    elif fare < 50:
+        return 4 # 40-50 pounds
+    elif fare < 100:
+        return 5 # 50-100 pounds
     else:
-        return '>100 pounds'
+        return 6 # >100 pounds
 
+def is_alone(family_size):
+    if family_size == 1:
+        return 1
+    else:
+        return 0
+
+# Create two new features
 df['AgeGroup'] = df['Age'].apply(age_groups)
 df['FareGroup'] = df['Fare'].apply(fare_groups)
+df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+df['IsAlone'] = df['FamilySize'].apply(is_alone)
 
-df.to_csv('titanic-training-dataset-cleaned.csv')
+# Delete Cabin, Name, Fare, Ticket and PassengerId column
+df.drop(['Cabin', 'Name', 'Ticket', 'Fare', 'PassengerId'], 1, inplace=True)
+
+# Remove two rows with null value at Embarked
+df.dropna(inplace=True)
+
+# Encode Embarked: S = 0 , C = 1, Q = 2
+df['Embarked'] = df['Embarked'].replace(['S','C','Q'], [0,1,2])
+
+df.to_csv(url + "-cleaned.csv", index=False)
